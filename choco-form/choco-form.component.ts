2951e934dc599ab1aaa4de5f6c7d9c21b514fed6
NandhiniSK <51111386+NandhiniSK@users.noreply.github.com>
2019-12-22 21:58:35 +0530

    FormComponent

---
+++ choco-form/choco-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choco-form',
+  templateUrl: './choco-form.component.html',
+  styleUrls: ['./choco-form.component.css']
+})
+export class ChocoFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
